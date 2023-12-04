3d071d855b973c6ec9b92a33062f9ee8cdd376e6
Beavermon <108250433+Beavermon@users.noreply.github.com>
2023-12-04 11:14:22 -0800

    coded isoceles

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 function isosceles () {
-    finch.setMove(MoveDir.Forward, 10, 50)
-    finch.setTurn(RLDir.Right, 90, 50)
-    finch.setMove(MoveDir.Forward, 10, 50)
-    finch.setTurn(RLDir.Right, 45, 50)
-    finch.setMove(MoveDir.Forward, 14.142, 50)
-    finch.setTurn(RLDir.Right, 45, 50)
+    finch.setMove(MoveDir.Forward, 10, 100)
+    finch.setTurn(RLDir.Right, 90, 100)
+    finch.setMove(MoveDir.Forward, 10, 100)
+    finch.setTurn(RLDir.Right, 135, 100)
+    finch.setMove(MoveDir.Forward, 14.142, 500)
+    finch.setTurn(RLDir.Right, 135, 500)
+    music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.UntilDone)
 }
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     isosceles()
